b8c6610470abb2e00589d831d251356624e736f7
Bia Astle <astlebia@gmail.com>
2025-08-23 14:42:25 -0300

    Add agenda_marketing and agenda_ia endpoints

---
+++ app/routers/agenda_ia.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, HTTPException
+from app.schema import EventoIACreate
+from app.database import SessionDep
+from app.models import EventoIA
+from sqlmodel import select
+
+router = APIRouter()
+
+@router.get("/eventos-rh", response_model=list[EventoIA])
+def read_eventos(session:SessionDep) -> list[EventoIA]:
+    eventos  = session.exec(select(EventoIA)).all()
+    return eventos
+
+@router.get("/eventos-rh/{evento_id}", response_model = EventoIA)
+def read_evento(evento_id:int, session:SessionDep) -> EventoIA:
+    evento = session.get(EventoIA, evento_id)
+    if not evento:
+        raise HTTPException(status_code=404, detail="Evento não encontrado")
+    return evento
+
+@router.post("/eventos-rh", response_model=EventoIA)
+def create_evento(evento:EventoIACreate, session:SessionDep) -> EventoIA:
+    new_evento = EventoIA(data_inicio = evento.data_inicio, data_fim = evento.data_fim, evento = evento.evento, descricao = evento.descricao, engajamento = evento.engajamento, status = evento.status)
+    session.add(new_evento)
+    session.commit()
+    session.refresh(new_evento)
+    return new_evento
+
+@router.put("/eventos-rh/{evento_id}", response_model= EventoIA)
+def update_evento(evento_id:int, evento_updated: EventoIACreate, session:SessionDep) -> EventoIA:
+    evento = session.get(EventoIA, evento_id)
+    if not evento:
+        raise HTTPException(status_code=404, detail="Evento não encontrado")
+    evento.data_inicio = evento_updated.data_inicio
+    evento.data_fim = evento_updated.data_fim
+    evento.evento = evento_updated.evento
+    evento.descricao = evento_updated.descricao
+    evento.engajamento = evento_updated.engajamento
+    evento.status = evento_updated.status
+    session.add(evento)
+    session.commit()
+    session.refresh(evento)
+    return evento
+
+@router.delete("/eventos-rh/{evento_id}", response_model=EventoIA)
+def delete_evento(evento_id:int, session:SessionDep) -> EventoIA:
+    evento = session.get(EventoIA, evento_id)
+    if not evento:
+        raise HTTPException (status_code=404, detail="Evento não encontrado")
+    session.delete(evento)
+    session.commit()
+    return evento
---
+++ app/routers/agenda_marketing.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, HTTPException
+from app.schema import EventoMarketingCreate
+from app.database import SessionDep
+from app.models import EventoMarketing
+from sqlmodel import select
+
+router = APIRouter()
+
+@router.get("/eventos-rh", response_model=list[EventoMarketing])
+def read_eventos(session:SessionDep) -> list[EventoMarketing]:
+    eventos  = session.exec(select(EventoMarketing)).all()
+    return eventos
+
+@router.get("/eventos-rh/{evento_id}", response_model = EventoMarketing)
+def read_evento(evento_id:int, session:SessionDep) -> EventoMarketing:
+    evento = session.get(EventoMarketing, evento_id)
+    if not evento:
+        raise HTTPException(status_code=404, detail="Evento não encontrado")
+    return evento
+
+@router.post("/eventos-rh", response_model=EventoMarketing)
+def create_evento(evento:EventoMarketingCreate, session:SessionDep) -> EventoMarketing:
+    new_evento = EventoMarketing(data_inicio = evento.data_inicio, data_fim = evento.data_fim, evento = evento.evento, descricao = evento.descricao, status = evento.status)
+    session.add(new_evento)
+    session.commit()
+    session.refresh(new_evento)
+    return new_evento
+
+@router.put("/eventos-rh/{evento_id}", response_model= EventoMarketing)
+def update_evento(evento_id:int, evento_updated: EventoMarketingCreate, session:SessionDep) -> EventoMarketing:
+    evento = session.get(EventoMarketing, evento_id)
+    if not evento:
+        raise HTTPException(status_code=404, detail="Evento não encontrado")
+    evento.data_inicio = evento_updated.data_inicio
+    evento.data_fim = evento_updated.data_fim
+    evento.evento = evento_updated.evento
+    evento.descricao = evento_updated.descricao
+    evento.status = evento_updated.status
+    session.add(evento)
+    session.commit()
+    session.refresh(evento)
+    return evento
+
+@router.delete("/eventos-rh/{evento_id}", response_model=EventoMarketing)
+def delete_evento(evento_id:int, session:SessionDep) -> EventoMarketing:
+    evento = session.get(EventoMarketing, evento_id)
+    if not evento:
+        raise HTTPException (status_code=404, detail="Evento não encontrado")
+    session.delete(evento)
+    session.commit()
+    return evento
