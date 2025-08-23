cd6082f329f7197dc65a3b979f29053d9055a5cb
Bia Astle <astlebia@gmail.com>
2025-08-23 15:30:56 -0300

    Include agenda_marketing and agenda_ia routes to main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from app.routers import agenda_rh
+from app.routers import agenda_rh,agenda_marketing,agenda_ia
 app = FastAPI()
 app.include_router(agenda_rh.router)
+app.include_router(agenda_marketing.router)
+app.include_router(agenda_ia.router)
 
 @app.get("/")
 async def root():
